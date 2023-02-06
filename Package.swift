9c4a822a517ff5ac3b166bbf205a8978651e666d
Geonhyeong Lim <20037035+gon125@users.noreply.github.com>
2023-02-06 22:09:07 +0900

    init

--- Package.swift
+++ Package.swift
@@ -4,12 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "NonViewLayer",
+    name: "MigrationLibraries",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NonViewLayer",
             targets: ["NonViewLayer"]),
+        .library(
+            name: "SwiftUIViews",
+            targets: ["SwiftUIViews"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,5 +28,8 @@ let package = Package(
         .testTarget(
             name: "NonViewLayerTests",
             dependencies: ["NonViewLayer"]),
+        .target(
+            name: "SwiftUIViews",
+            dependencies: [])
     ]
 )
---
+++ Sources/SwiftUIViews/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Gon on 2023/02/06.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
